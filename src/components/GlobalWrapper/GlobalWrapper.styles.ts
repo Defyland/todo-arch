467d0ed8c2b759a3d2fc6fc9d82a5239d375442f
Defyland <allanflaviosilva@gmail.com>
2024-08-29 20:40:23 -0300

    feat: add new layout configurations for component

--- src/components/GlobalWrapper/GlobalWrapper.styles.ts
+++ src/components/GlobalWrapper/GlobalWrapper.styles.ts
@@ -1,4 +1,4 @@
 export const Styles = {
-    Container: 'flex min-h-screen justify-center bg-white',
+    Container: 'flex min-h-screen justify-center',
     Content: 'flex min-h-screen w-[1366px] px-4',
 };
