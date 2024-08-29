eb5160925c148ce6cdda35fec56187b42764621d
Defyland <allanflaviosilva@gmail.com>
2024-08-29 20:42:53 -0300

    feat: add conditional rendering for Header component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,18 +1,24 @@
 'use client';
 
-import {IApp} from '@/components/App/App.types';
-import {GlobalWrapper} from '@/components/GlobalWrapper';
-import {Header} from '@/components/Header';
-import {Provider} from 'react-redux';
+import { usePathname } from 'next/navigation';
+import { IApp } from '@/components/App/App.types';
+import { GlobalWrapper } from '@/components/GlobalWrapper';
+import { Header } from '@/components/Header';
+import { Provider } from 'react-redux';
 import store from '@/redux/store';
 
-export const App = ({children}: IApp.IView) => (
-    <div data-testid={`App`}>
-        <Provider store={store}>
-            <Header />
-            <GlobalWrapper.Container>
-                <GlobalWrapper.Content>{children}</GlobalWrapper.Content>
-            </GlobalWrapper.Container>
-        </Provider>
-    </div>
-);
+export const App = ({ children }: IApp.IView) => {
+    const pathname = usePathname();
+    const isHomePage = pathname === '/list';
+
+    return (
+        <div data-testid={`App`}>
+            <Provider store={store}>
+                {isHomePage ? <Header /> : null }
+                <GlobalWrapper.Container>
+                    <GlobalWrapper.Content>{children}</GlobalWrapper.Content>
+                </GlobalWrapper.Container>
+            </Provider>
+        </div>
+    );
+};
